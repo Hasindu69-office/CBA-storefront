7110ae215393720e533f96c25e11c79d8ada330e
Hasindu69-office <hasindu@graphics.lk>
2026-09-01 09:42:37 +0530

    Implemented meta pixel to frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { getBaseURL } from "@lib/util/env"
 import { Metadata } from "next"
+import Script from "next/script"
 import NotificationProvider from "@modules/common/components/notification-provider"
 import RouteScrollRestoration from "@modules/layout/components/route-scroll-restoration"
 import "styles/globals.css"
@@ -24,6 +25,29 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1361822756100417');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1361822756100417&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <main className="relative w-full min-h-screen bg-white">{props.children}</main>
         <RouteScrollRestoration />
         <NotificationProvider />
